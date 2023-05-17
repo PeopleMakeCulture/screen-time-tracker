4e47a59a0656c6b5222a8b049eca89b5c1019297
Jing - Peters MBP <jingcao.yale@gmail.com>
2023-05-17 17:56:15 -0400

    add Title and Table components

--- src/App.js
+++ src/App.js
@@ -1,14 +1,27 @@
 import logo from './logo.svg';
 import './App.css';
 
-function App() {
+import React, { useState } from 'react';
+import Table from './Table';
+import Title from './Title';
+
+const App = () => {
+
+  const seedData = [
+    { id: 1, name: 'John', age: 25 },
+    { id: 2, name: 'Jane', age: 30 },
+    { id: 3, name: 'Bob', age: 28 },
+    { id: 4, name: 'Alice', age: 35 },
+  ];
+
+  const [ tableData, setTableData ] = useState(seedData)
+
   return (
     <div className="App">
       <header className="App-header">
+        <Title />
+        <Table data = {tableData} />
         <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
         <a
           className="App-link"
           href="https://reactjs.org"
---
+++ src/Title.js
@@ -0,0 +1,15 @@
+import React, { Component } from 'react';
+
+class Title extends Component {
+  render() {
+    return (
+      <div>
+      <h1>Welcome to my Screen Time App!</h1>
+      <p>This is a description of my screen time app.</p>
+    </div>
+
+    );
+  }
+}
+
+export default Title;
